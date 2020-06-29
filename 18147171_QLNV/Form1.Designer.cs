a8b9110a947fbcb2c0e9b124d7b281cb014bb528
Chiến Phùng <18147171@student.hcmute.edu.vn>
2020-06-29 14:16:03 +0700

    First Commit

---
+++ 18147171_QLNV/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace _18147171_QLNV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.oPPDataSet = new _18147171_QLNV.OPPDataSet();
+            this.qlnv1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qlnv1TableAdapter = new _18147171_QLNV.OPPDataSetTableAdapters.qlnv1TableAdapter();
+            this.emIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deptDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtemID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btLuuFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oPPDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnv1BindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.emIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.deptDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.qlnv1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 242);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(306, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // oPPDataSet
+            // 
+            this.oPPDataSet.DataSetName = "OPPDataSet";
+            this.oPPDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // qlnv1BindingSource
+            // 
+            this.qlnv1BindingSource.DataMember = "qlnv1";
+            this.qlnv1BindingSource.DataSource = this.oPPDataSet;
+            // 
+            // qlnv1TableAdapter
+            // 
+            this.qlnv1TableAdapter.ClearBeforeFill = true;
+            // 
+            // emIDDataGridViewTextBoxColumn
+            // 
+            this.emIDDataGridViewTextBoxColumn.DataPropertyName = "emID";
+            this.emIDDataGridViewTextBoxColumn.HeaderText = "emID";
+            this.emIDDataGridViewTextBoxColumn.Name = "emIDDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // deptDataGridViewTextBoxColumn
+            // 
+            this.deptDataGridViewTextBoxColumn.DataPropertyName = "Dept";
+            this.deptDataGridViewTextBoxColumn.HeaderText = "Dept";
+            this.deptDataGridViewTextBoxColumn.Name = "deptDataGridViewTextBoxColumn";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtemID);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtDept);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 132);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "emID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dept";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Find";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Result";
+            // 
+            // txtemID
+            // 
+            this.txtemID.Location = new System.Drawing.Point(80, 19);
+            this.txtemID.Name = "txtemID";
+            this.txtemID.Size = new System.Drawing.Size(129, 20);
+            this.txtemID.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 58);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(80, 89);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(129, 20);
+            this.txtDept.TabIndex = 7;
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(129, 187);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(100, 20);
+            this.txtFind.TabIndex = 8;
+            this.txtFind.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFind_KeyUp);
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(39, 150);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 8;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btEdit
+            // 
+            this.btEdit.Location = new System.Drawing.Point(154, 150);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(75, 23);
+            this.btEdit.TabIndex = 9;
+            this.btEdit.Text = "Edit";
+            this.btEdit.UseVisualStyleBackColor = true;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(270, 150);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 10;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btLuuFile
+            // 
+            this.btLuuFile.Location = new System.Drawing.Point(380, 150);
+            this.btLuuFile.Name = "btLuuFile";
+            this.btLuuFile.Size = new System.Drawing.Size(75, 23);
+            this.btLuuFile.TabIndex = 11;
+            this.btLuuFile.Text = "Luu File";
+            this.btLuuFile.UseVisualStyleBackColor = true;
+            this.btLuuFile.Click += new System.EventHandler(this.btLuuFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 395);
+            this.Controls.Add(this.btLuuFile);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btEdit);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oPPDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnv1BindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private OPPDataSet oPPDataSet;
+        private System.Windows.Forms.BindingSource qlnv1BindingSource;
+        private OPPDataSetTableAdapters.qlnv1TableAdapter qlnv1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deptDataGridViewTextBoxColumn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtemID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btLuuFile;
+    }
+}
+
---
+++ 18147171_QLNV/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Linq;
+using System.IO;
+
+namespace _18147171_QLNV
+{
+    public partial class Form1 : Form
+    {
+        qlnv1 ql = new qlnv1();
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'oPPDataSet.qlnv1' table. You can move, or remove it, as needed.
+            //this.qlnv1TableAdapter.Fill(this.oPPDataSet.qlnv1);
+            var list = dataGridView1.DataSource = db.qlnv1s.Select(s => s).ToList();
+            dataGridView1.Refresh();
+
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            qlnv1 ql = new qlnv1();
+            try
+            {
+                ql.emID = txtemID.Text;
+                ql.Name = txtName.Text;
+                ql.Dept = txtDept.Text;
+                db.qlnv1s.InsertOnSubmit(ql);
+                db.SubmitChanges();
+                Form1_Load(sender, e);
+            }
+            catch(DuplicateKeyException ie)
+            {
+                MessageBox.Show(ie.Message, "Thong Bao", MessageBoxButtons.OK);
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.Message, "Thong Bao", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btEdit_Click(object sender, EventArgs e)
+        {
+            //qlnv1 ql = new qlnv1();
+            try
+            {
+                ql = db.qlnv1s.Where(p => p.emID == txtemID.Text).Single();
+                ql.Name = txtName.Text;
+                ql.Dept = txtDept.Text;
+                db.SubmitChanges();
+                Form1_Load(sender, e);
+            }
+            catch(InvalidOperationException ne)
+            {
+                MessageBox.Show(ne.Message, "Thong Bao", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            //qlnv1 ql = new qlnv1();
+            try
+            {
+                ql = db.qlnv1s.Where(p => p.emID == txtemID.Text).Single();
+                ql.Name = txtName.Text;
+                ql.Dept = txtDept.Text;
+                db.qlnv1s.DeleteOnSubmit(ql);
+                db.SubmitChanges();
+                Form1_Load(sender, e);
+            }
+            catch (InvalidOperationException ne)
+            {
+                MessageBox.Show(ne.Message, "Thong Bao", MessageBoxButtons.OK);
+            }
+        }
+
+        private void txtFind_KeyUp(object sender, KeyEventArgs e)
+        {
+            var list = dataGridView1.DataSource = (from s in db.qlnv1s where s.emID.Contains(txtFind.Text) select s).ToList();
+            txtemID.DataBindings.Clear();
+            txtName.DataBindings.Clear();
+            txtDept.DataBindings.Clear();
+            txtemID.DataBindings.Add("text", list, "emID");
+            txtName.DataBindings.Add("text", list, "Name");
+            txtDept.DataBindings.Add("text", list, "Dept");
+        }
+
+        private void btLuuFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"C:\Users\User\source\repos\18147171_QLNV\congchien.txt";
+                var list_yr = (from s in db.qlnv1s /*where s.DOB.value >= dTP02*/ select s).ToList();
+                dataGridView1.DataSource = list_yr;
+
+                TextWriter wrt = new StreamWriter(path);
+                wrt.WriteLine("  ID Number \t\t\t      Name \t\t\t\t     Deparment");
+                for(int i=0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for(int j=0;j <dataGridView1.Rows.Count; j++)
+                    {
+                        wrt.Write("\t" + dataGridView1.Rows[i].Cells[j].Value.ToString()+"|");
+                    }
+                    wrt.WriteLine("");
+                    wrt.WriteLine("=================================================================================================");
+                }
+                wrt.Close();
+                MessageBox.Show("Xem ket qua", "Thong Bao", MessageBoxButtons.OK);
+            }
+            catch(FileLoadException fe)
+            {
+                MessageBox.Show(fe.Message, "Thong Bao", MessageBoxButtons.OK);
+            }
+            catch(ArgumentOutOfRangeException ae)
+            {
+                MessageBox.Show(ae.Message, "Thong bao", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
